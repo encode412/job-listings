508c7777c35cf6e9cd6dee571af028b286102f53
encodedev <timothyonyea@gmail.com>
2025-01-22 12:13:47 +0100

    initial commit: router setup and home page ui implementation

---
+++ src/App2.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "john doe",
+      status: "active",
+      tasks: ["Task One", "Task Two", "Task Three"],
+      link: "https://www.google.com",
+    };
+  },
+  methods: {
+    toggleStatus() {
+      this.status = this.status === "active" ? "pending" : "active";
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is not active</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click for google</a> -->
+  <a :href="link">Click for google</a>
+  <br />
+  <br />
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+</template>
