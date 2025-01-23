6bdcb3adae84443533d6e87b567166e6cfc56915
encodedev <timothyonyea@gmail.com>
2025-01-23 09:21:52 +0100

    feat: job delete functionality implemented

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,18 +1,33 @@
 <script setup>
-import { RouterLink, useRoute } from "vue-router";
+import { RouterLink, useRoute, useRouter } from "vue-router";
 import { reactive, onMounted } from "vue";
 import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 import axios from "axios";
 import BackButton from "@/components/BackButton.vue";
+import { useToast } from "vue-toastification";
 
 const route = useRoute();
-
+const router = useRouter();
 const jobId = route.params.id;
-
+const toast = useToast();
 const state = reactive({
   job: {},
   isLoading: true,
 });
+
+const deleteJob = async () => {
+  try {
+    const confirm = window.confirm("Are you sure you want to delete this job?");
+    if (confirm) {
+      await axios.delete(`/api/jobs/${jobId}`);
+      toast.success("Job deleted successfully");
+      router.push("/jobs");
+    }
+  } catch (error) {
+    toast.error("Error deleting job");
+    console.log("Error deleting job", error);
+  }
+};
 
 onMounted(async () => {
   try {
@@ -40,9 +55,7 @@ onMounted(async () => {
             <div
               class="text-gray-500 mb-4 flex align-middle items-center justify-center md:justify-start"
             >
-              <i
-                class="pi pi-map-marker text-xl text-orange-700 mr-2"
-              ></i>
+              <i class="pi pi-map-marker text-xl text-orange-700 mr-2"></i>
               <p class="text-orange-700">{{ state.job.location }}</p>
             </div>
           </div>
@@ -98,6 +111,7 @@ onMounted(async () => {
               >Edit Job</RouterLink
             >
             <button
+              @click="deleteJob"
               class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
             >
               Delete Job
