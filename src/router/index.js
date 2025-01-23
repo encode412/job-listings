64b940ed5ac1991052e6902bc87e0d1945c7c179
encodedev <timothyonyea@gmail.com>
2025-01-23 09:37:15 +0100

    feat: edit job functionality

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobView from "@/views/JobView.vue";
 import AddJobView from "@/views/AddJobView.vue";
+import EditJobView from "@/views/EditJobView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,11 +23,16 @@ const router = createRouter({
       path: "/job/:id",
       name: "job",
       component: JobView,
-    },
+    },    
     {
       path: "/jobs/add",
       name: "add-job",
       component: AddJobView,
+    },
+    {
+      path: "/jobs/edit/:id",
+      name: "edit-job",
+      component: EditJobView,
     },
     {
       path: "/:catchAll(.*)",
